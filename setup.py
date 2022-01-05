99c9d20aa66f9ef9cc004b3acdad56e8f8a86394
addyett <g.aditya2048@gmail.com>
2022-01-05 10:50:06 +0530

    add setup files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+    name = 'video_player',
+    packages = ['video_player'],
+    version = '1.0.0',
+    license='MIT',
+    description = 'a simple video player written in python using ffpyplayer and OpenCV',
+    author = 'addyett',
+    author_email = 'g.aditya2048@gmail.com',
+    url = 'https://github.com/addyett/video-player',
+    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords = ['OpenCV', 'ffpyplayer', 'media-player', 'video'],
+    install_requires=[
+            'opencv-python',
+            'numpy',
+            'ffpyplayer',
+            'pillow'
+    ],
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    ]
+)
