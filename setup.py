3a96c584889319e9d2dccc5a0c337e6e7ffbd211
addyett <g.aditya2048@gmail.com>
2022-01-05 23:12:25 +0530

    version bump to 1.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="cvplayer",
-    version="1.0.0",
+    version="1.1.0",
     description="a simple video player written in python using ffpyplayer and OpenCV",
     long_description=README,
     long_description_content_type="text/markdown",
